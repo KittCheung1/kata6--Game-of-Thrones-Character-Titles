c77081ea1ea6b8a4054e56ce58106bda06f3a693
Kitt Cheung <kitt.cheung@se.experis.com>
2022-01-20 14:00:59 +0100

    2nd commit: unfinished Kata

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function correctTitle(input) {
-    let splittedInput = input.toLowerCase().split(/([,.\s])/).join("").split(" ");
+    let splittedInput = input.toLowerCase().split(/([,.\s])/).join(" ");
     let arr = [];
+    console.log(splittedInput);
     // for (const words of splittedInput) {
     //     words[0].toUpperCase() + words[i].substr(1);
     //     console.log(words);
@@ -13,17 +14,19 @@ function correctTitle(input) {
     // }
     for (let i = 0; i < splittedInput.length; i++) {
         splittedInput[i] = splittedInput[i].charAt(0).toUpperCase() + splittedInput[i].substr(1);
+        // console.log(splittedInput[i]);
         arr.push(splittedInput[i]);
         // console.log(arr[i]);
-        if (arr[i] == ("In" || "The" || "Of" || "And")) {
-            arr[i] = arr[i].map(toLowerCase());
-            arr.push(arr[i]);
-            // console.log(splittedInput[i]);
+        if (arr[i] === "In" | arr[i] === "The" | arr[i] === "Of" | arr[i] === "And") {
+            arr[i] = arr[i].toLowerCase();
+            // arr.push(arr[i]);
 
         }
-        console.log(arr[i]);
+        // console.log(arr[i]);
     }
+    // console.log(arr);
 }
 
 correctTitle("jOn SnoW, kINg IN thE noRth");
 correctTitle("sansa stark,lady of winterfell.");
+correctTitle("TYRION LANNISTER, HAND OF THE QUEEN.");
